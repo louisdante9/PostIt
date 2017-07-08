c1aa00065c47e5ad43be4130b5cff3af4fff6fe4
louis <louisdante9@gmial.com>
2017-07-08 15:09:03 +0100

    add list method to group controller

--- server/controller/groups.js
+++ server/controller/groups.js
@@ -9,4 +9,10 @@ module.exports = {
       .then(group => res.status(201).json(group))
       .catch(error => res.status(400).send(error));
   },
+  list(req, res) {
+  return Group
+    .all()
+    .then(groups => res.status(200).send(groups))
+    .catch(error => res.status(400).send(error));
+  },
 };
