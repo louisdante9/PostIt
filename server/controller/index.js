28b4ea57ed8a58e5f4bb8cd9084c8a6af58cf4c7
louisdante9 <louisdante9@gmail.com>
2017-10-09 12:55:59 +0100

    update node package

--- server/controller/index.js
+++ server/controller/index.js
@@ -3,10 +3,4 @@ import Users from './users';
 import Messages from './messages';
 import GroupUsers from './groupuser';
 
-// module.exports = {
-//   Groups,
-//   GroupUsers,
-//   Users,
-//   Messages
-// };
 export  { Users, Groups, GroupUsers, Messages};
