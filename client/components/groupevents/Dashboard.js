4efdf9d756cac0456f3bdc0bbecc109aea8f0b02
louisdante9 <louisdante9@gmail.com>
2017-10-13 16:39:37 +0100

    refactor dashboard.js for jsdoc and eslint length rule

--- client/components/groupevents/Dashboard.js
+++ client/components/groupevents/Dashboard.js
@@ -1,102 +1,170 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
-import { addFlashMessage} from '../../actions/flashMessages';
+import { addFlashMessage } from '../../actions/flashMessages';
 import Aside from './Aside';
 import Message from './Message';
 import MessageBox from './MessageBox';
 import Modal from './modal';
 import UserModal from './userModal';
 import Welcome from './welcome';
+import { getGroups, createGroup, getMessages, createMessage }
+  from '../../actions/groupAction';
 
-import { getGroups, createGroup, getMessages, createMessage } from '../../actions/groupAction';
-
+/**
+ * 
+ * 
+ * @class Dashboard
+ * @extends {Component}
+ */
 class Dashboard extends Component {
-  constructor(props){
+  /**
+   * Creates an instance of Dashboard.
+   * @param {any} props 
+   * @memberof Dashboard
+   */
+  constructor(props) {
     super(props);
     this.state = {
-        groupId: '',
-        message: '',
-        flag: 'normal',
-        msg: {},
-        introjs: false
+      groupId: '',
+      message: '',
+      flag: 'normal',
+      msg: {},
+      introjs: false
     };
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
-    this.setGroupMessages =  this.setGroupMessages.bind(this);
+    this.setGroupMessages = this.setGroupMessages.bind(this);
     this.openSlideAdduser = this.openSlideAdduser.bind(this);
     this.onClickTour = this.onClickTour.bind(this);
   }
 
+  /**
+   * 
+   * 
+   * @memberof Dashboard
+   * @returns {void}
+   */
   componentDidMount() {
-    this.props.getGroups();    
+    this.props.getGroups();
   }
-  componentDidUpdate () {
-    if(this.state.introjs) {
-      introJs().start();
-    }
-  }
-onClickTour(event){
-  event.preventDefault();
-  this.setState({introjs: true});
-}
 
-  setGroupMessages(id){
+  /**
+   * 
+   * 
+   * @param {any} event 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  onClickTour(event) {
+    event.preventDefault();
+    this.setState({ introjs: true });
+  }
+
+  /**
+   * 
+   * 
+   * @param {any} id 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  setGroupMessages(id) {
     return (evt) => {
       evt.preventDefault();
       this.props.getMessages(id);
       this.setState({ groupId: id });
     };
   }
-//update new messages coming through to the client 
- handleCallback(msg) {
 
- }
 
-//handles submit event 
-  onSubmit(event){
+  /**
+   * 
+   * 
+   * @param {any} event 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  onSubmit(event) {
     event.preventDefault();
     const { userId, username } = this.props.user;
-    const data = Object.assign({}, this.state, { userId, username});
+    const data = Object.assign({}, this.state, { userId, username });
     this.props.createMessage(this.state.groupId, data);
-    this.setState({message:''});
+    this.setState({ message: '' });
   }
 
-  openSlideAdduser(){
+  /**
+   * 
+   * 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  openSlideAdduser() {
     $('select').material_select();
     $('.modal').modal();
   }
 
-  onChange(event){
+  /**
+   * 
+   * 
+   * @param {any} event 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  onChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
     });
   }
 
-  showModal(){
+  /**
+   * 
+   * 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  showModal() {
     $('select').material_select();
     $('.modal').modal();
   }
 
-  getGroupName(evn){
+  /**
+   * 
+   * 
+   * @param {any} evn 
+   * @returns {void}
+   * @memberof Dashboard
+   */
+  getGroupName(evn) {
     return (evt) => {
       return evn;
     };
   }
 
+  /**
+   * 
+   * 
+   * @param {any} messages 
+   * @param {any} groups 
+   * @param {any} GroupName 
+   * @returns {void}
+   * @memberof Dashboard
+   */
   renderMessageBoard(messages, groups, GroupName) {
     return (
       <div>
         <hr className="grey-text text-lighten-2" />
         <div className="collection-item avatar">
-          <p className="email-subject truncate"><span className="email-tag grey lighten-3">{GroupName && GroupName.name}</span>
-            <a href="#modal2" className="secondary-content modal-trigger" onClick={(event) => {
-              event.preventDefault();
-              this.openSlideAdduser();
-            }}>
+          <p className="email-subject truncate">
+            <span className="email-tag grey lighten-3">
+              {GroupName && GroupName.name}
+            </span>
+            <a href="#modal2" className="secondary-content modal-trigger"
+              onClick={(event) => {
+                event.preventDefault();
+                this.openSlideAdduser();
+              }}>
               click to add user
-        
+
                 Add User
         </a>
           </p>
@@ -111,97 +179,133 @@ onClickTour(event){
               </ul>
             </div>
             <div className="col s2 m2 l2 email-actions">
-              <a href="#!"><span><i className="mdi-content-reply"></i></span></a>
-              <a href="#!"><span><i className="mdi-navigation-more-vert"></i></span></a>
+              <a href="#!">
+                <span>
+                  <i className="mdi-content-reply"></i>
+                </span>
+              </a>
+              <a href="#!">
+                <span>
+                  <i className="mdi-navigation-more-vert"></i>
+                </span>
+              </a>
             </div>
           </div>
         </div>
         <hr />
         {/**  message box */}
-        <MessageBox message={this.state.message} flag={this.state.flag} onChange={this.onChange} onSubmit={this.onSubmit} groups={groups} />
+        <MessageBox message={this.state.message}
+          flag={this.state.flag} onChange={this.onChange}
+          onSubmit={this.onSubmit} groups={groups} />
       </div>
     );
   }
-  
-    render() {
-      const { groups, allMsgs } = this.props;
-      const messages = allMsgs[this.state.groupId] || [];
-      const GroupName = groups.find(group => group.id === this.state.groupId);
-        return (
+
+  /**
+   * 
+   * 
+   * @memberof Dashboard
+   * @returns {void}
+   */
+  render() {
+    const { groups, allMsgs } = this.props;
+    const messages = allMsgs[this.state.groupId] || [];
+    const GroupName = groups.find(group => group.id === this.state.groupId);
+    return (
+      <div>
+        <section id="content">
           <div>
-            <section id="content">
-              <div>
-                <div id="mail-app" className="section">
-                  <div className="row">
-                    <div className="col s12">
-                      {/** aside **/}
-                      <Aside
-                        showModal={this.showModal}
-                        groups={groups}
-                        setGroupMessages={this.setGroupMessages}
-                      />
-                      <div id="email-details" className="col s12 m8 l8 card-panel card-1">
-                      {this.state.groupId ? 
-                        (
-                          <div>
-                            <hr className="grey-text text-lighten-2" />
-                            <div className="collection-item avatar">
-                              <p className="email-subject truncate"><span className="email-tag grey lighten-3">{GroupName && GroupName.name}</span>
-                                <a href="#modal2" className=" modal-trigger" onClick={(event) => {
+            <div id="mail-app" className="section">
+              <div className="row">
+                <div className="col s12">
+                  {/** aside **/}
+                  <Aside
+                    showModal={this.showModal}
+                    groups={groups}
+                    setGroupMessages={this.setGroupMessages}
+                  />
+                  <div id="email-details"
+                    className="col s12 m8 l8 card-panel card-1">
+                    {this.state.groupId ?
+                      (
+                        <div>
+                          <hr className="grey-text text-lighten-2" />
+                          <div className="collection-item avatar">
+                            <p className="email-subject truncate">
+                              <span className="email-tag grey lighten-3">
+                                {GroupName && GroupName.name}
+                              </span>
+                              <a href="#modal2" className=" modal-trigger"
+                                onClick={(event) => {
                                   event.preventDefault();
                                   this.openSlideAdduser();
                                 }}>
-                                                              
-                            <span className="send" data-intro="Add users here">
-                                    Add User
-                            </span></a>
-                            {/**<span className="send"  onClick={this.onClickTour}>Take a tour</span>  */} 
-                              </p>
-                              <hr />
-                            </div>
-                            <div id="message-board" className="email-content-wrap">
-                              <div className="row">
-                                <div className="col s10 m10 l10">
-                                  <ul className="collection">
-                                    {/** single message **/}
-                                    <Message messages={messages} groups={groups} />
-                                  </ul>
-                                </div>
-                                <div className="col s2 m2 l2 email-actions">
-                                  <a href="#!"><span><i className="mdi-content-reply"></i></span></a>
-                                  <a href="#!"><span><i className="mdi-navigation-more-vert"></i></span></a>
-                                </div>
+
+                                <span className="send"
+                                  data-intro="Add users here">
+                                  Add User
+                            </span>
+                              </a>
+                            </p>
+                            <hr />
+                          </div>
+                          <div id="message-board" c
+                            lassName="email-content-wrap">
+                            <div className="row">
+                              <div className="col s10 m10 l10">
+                                <ul className="collection">
+                                  {/** single message **/}
+                                  <Message messages={messages}
+                                    groups={groups} />
+                                </ul>
+                              </div>
+                              <div className="col s2 m2 l2 email-actions">
+                                <a href="#!">
+                                  <span>
+                                    <i className="mdi-content-reply">
+                                    </i>
+                                  </span>
+                                </a>
+                                <a href="#!">
+                                  <span><i className="mdi-navigation-more-vert">
+                                  </i>
+                                  </span>
+                                </a>
                               </div>
                             </div>
-                            <hr />
-                            {/**  message box */}
-                            <MessageBox message={this.state.message} flag={this.state.flag} onChange={this.onChange} onSubmit={this.onSubmit} groups={groups} onClick={this.onClickTour} />
                           </div>
-                        )        
+                          <hr />
+                          {/**  message box */}
+                          <MessageBox message={this.state.message}
+                            flag={this.state.flag} onChange={this.onChange}
+                            onSubmit={this.onSubmit} groups={groups}
+                            onClick={this.onClickTour} />
+                        </div>
+                      )
                       : <Welcome />}
 
-                      </div>
-                    </div>
-
                   </div>
-                  <Modal createGroup={this.props.createGroup} />
                 </div>
-                <UserModal href="#modal2" group={this.state.groupId} />
+
               </div>
-            </section>
+              <Modal createGroup={this.props.createGroup} />
+            </div>
+            <UserModal href="#modal2" group={this.state.groupId} />
           </div>
-        );
-    }
+        </section>
+      </div>
+    );
+  }
 }
 Dashboard.propTypes = {
   createGroup: React.PropTypes.func.isRequired,
-  getGroups : React.PropTypes.func.isRequired,
+  getGroups: React.PropTypes.func.isRequired,
   getMessages: React.PropTypes.func.isRequired,
   createMessage: React.PropTypes.func.isRequired,
   groups: React.PropTypes.array.isRequired,
   messages: React.PropTypes.array.isRequired,
   user: React.PropTypes.object.isRequired,
-  active : React.PropTypes.bool.isRequired,
+  active: React.PropTypes.bool.isRequired,
   allMsgs: React.PropTypes.object.isRequired
 };
 
@@ -214,7 +318,5 @@ const mapStateToProps = (state) => {
 };
 
 
-
-
-
-export default connect(mapStateToProps, { getGroups, createGroup, getMessages, createMessage })(Dashboard);
+export default connect(mapStateToProps,
+  { getGroups, createGroup, getMessages, createMessage })(Dashboard);
