09b7db94c375410bd922af925e9e6abc86e18d7e
louisdante9 <louisdante9@gmail.com>
2017-10-13 16:39:57 +0100

    fix message.js jsdoc

--- client/components/groupevents/Message.js
+++ client/components/groupevents/Message.js
@@ -4,39 +4,39 @@ import moment from 'moment';
 
 export const Component = ({ messages, groups }) => {
 
-    const emptyMessage = (
-        <p>There are no meessages yet in this group</p>
-    );
+  const emptyMessage = (
+    <p>There are no meessages yet in this group</p>
+  );
 
-    const renderMessage = message => {
-        return (
-            <li key={message.id} className="collection-item avatar">
-                <img src="images/avatar.jpg" alt="" className="circle" />
-                <span>
-                    <span className="username">{message.User.username}
-                    </span>
-                    <span className="ultra-small grey-text time-text">
-                        {moment(message.createdAt).fromNow()}
-                    </span>
-                </span>
-                <p className="truncate black-text message-text">
-                    {message.message}
-                </p>
-            </li>
-        );
-    };
-
+  const renderMessage = message => {
     return (
-        <div className="messages">
-            {
-                messages.length > 0 ? 
-                messages.map(message => renderMessage(message)) : emptyMessage
-            }
-        </div>
+      <li key={message.id} className="collection-item avatar">
+        <img src="images/avatar.jpg" alt="" className="circle" />
+        <span>
+          <span className="username">{message.User.username}
+          </span>
+          <span className="ultra-small grey-text time-text">
+            {moment(message.createdAt).fromNow()}
+          </span>
+        </span>
+        <p className="truncate black-text message-text">
+          {message.message}
+        </p>
+      </li>
     );
+  };
+
+  return (
+    <div className="messages">
+      {
+        messages.length > 0 ?
+          messages.map(message => renderMessage(message)) : emptyMessage
+      }
+    </div>
+  );
 };
 Component.propTypes = {
-    messages: React.PropTypes.array.isRequired,
-    groups: React.PropTypes.array.isRequired
+  messages: React.PropTypes.array.isRequired,
+  groups: React.PropTypes.array.isRequired
 };
 export default Component;
