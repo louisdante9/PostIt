8f6ecefcb219715d0249f8ab2d5eba8296bee343
louisdante9 <louisdante9@gmail.com>
2017-12-11 11:08:54 +0100

    [Chore ] remove dead code

--- client/components/auth/Home.jsx
+++ client/components/auth/Home.jsx
@@ -4,30 +4,30 @@ import Footer from '../common/Footer.jsx';
 /* * 
  * 
  * @param {any} props 
- * @returns 
+ * @returns { void }
  */
 export const Home = (props) => {
-    return (
-        <div className="home-background-image">
-        <div className="home-container container align">
-            <div className="signincontainer">
-                <h1 className="cover-heading">PostIt.</h1>
-                <p className="intro-word">
-                    A simple application that allows friends and colleagues
-                        create groups for collaboration.
-                        This way you can post notifications with
-                        degree of priority to everyone in your group
-                        by sending a message once. Awesome isn't it
-                    </p>
-                <p className="">
-                    <a href="/signup"
-                        className=" btn shadow-effect home-btn">
-                        Get Started
-                    </a>
-                </p>
-            </div>
-            <Footer />
+  return (
+    <div className="home-background-image">
+      <div className="home-container container align">
+        <div className="signincontainer">
+          <h1 className="cover-heading">PostIt.</h1>
+          <p className="intro-word">
+            A simple application that allows friends and colleagues
+            create groups for collaboration.
+            This way you can post notifications with
+            degree of priority to everyone in your group
+            by sending a message once. Awesome isn't it
+          </p>
+          <p className="">
+            <a href="/signup"
+              className=" btn shadow-effect home-btn">
+              Get Started
+            </a>
+          </p>
         </div>
-        </div>
-    );
+        <Footer />
+      </div>
+    </div>
+  );
 };
